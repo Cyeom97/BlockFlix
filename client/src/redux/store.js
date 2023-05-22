9595560858428b29a3beeedb3d2335b070ce281b
Christopher Yeom <chrisyeom97@gmail.com>
2023-05-21 21:45:31 -0400

    finish remainding redux feature files

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './features/userSlice'
 import themeModeSlice from './features/themeModeSlice'
+import authModelSlice from './features/authModelSlice'
+import appStateSlice from './features/appStateSlice'
+import globalLoadingSlice from './features/globalLoadingSlice'
 
 const store = configureStore({
   reducer: {
     user: userSlice,
-    themeMode: themeModeSlice
+    themeMode: themeModeSlice,
+    authModel: authModelSlice,
+    appState: appStateSlice,
+    globalLoading: globalLoadingSlice
   }
 })
 
