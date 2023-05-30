55ba8ca6a23bf8216fbf9645a9120ed4f25fb9b2
Christopher Yeom <chrisyeom97@gmail.com>
2023-05-29 20:10:58 -0400

    routes

---
+++ client/src/components/layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+import { OutLet } from 'react-router-dom';
+
+const MainLayout = () => {
+    return (
+        <>
+            <Box display='flex' minHeight='100vh'>
+                <Box component='main'
+                flexGrow={1}
+                overflow='hidden'
+                minHeight='100vh'>
+                    <OutLet />
+
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+export default MainLayout;
