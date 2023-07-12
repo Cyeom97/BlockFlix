da8b7746f0791e41d395d537d9fa888e164de0bc
Christopher Yeom <chrisyeom97@gmail.com>
2023-07-11 21:12:22 -0400

    media detail page

--- client/src/pages/MediaDetail.jsx
+++ client/src/pages/MediaDetail.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+
+import { LoadingButton } from '@mui/lab';
+import { Box, Button, Chip, Divider, Stack, Typography } from '@mui/material';
+import { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import CircularRate from '../components/common/CircularRate';
+import Container from '../components/common/Container';
+import ImageHeader from '../components/common/ImageHeader';
+
+import uiConfigs from '../configs/ui.config';
+import tmdbConfigs from '../api/configs/tmdb.configs';
+import mediaApi from '../api/modules/media.api';
+import favoriteApi from '../api/modules/favorite.api';
 
 const MediaDetail = () => {
+    const { mediaType, mediaId } = useParams();
+
+    const {user, listFavorites} = useSelector((state) => state.user);
+
+    const [media, setMedia] = useState();
+
     return (
         <div>
         <h1>MediaDetail Page</h1>
