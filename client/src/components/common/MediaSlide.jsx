d8c6ebc3b281f79e715eabe9922082d3a5beab20
Christopher Yeom <chrisyeom97@gmail.com>
2023-07-30 21:58:39 -0400

    updated movie and tv pages

--- client/src/components/common/MediaSlide.jsx
+++ client/src/components/common/MediaSlide.jsx
@@ -7,29 +7,31 @@ import MediaItem from "./MediaItem";
 
 const MediaSlide = ({ mediaType, mediaCategory }) => {
     const [medias, setMedias] = useState([]);
-
+  
     useEffect(() => {
-        const getMedias = async () => {
-            const {response, err} = await mediaApi.getList({
-                mediaType,
-                mediaCategory,
-                page: 1
-            })
-            if (response) setMedias(response.results);
-            if (err) toast.error(err.message);  
-        }
-
-        getMedias();
+      const getMedias = async () => {
+        const { response, err } = await mediaApi.getList({
+          mediaType,
+          mediaCategory,
+          page: 1
+        });
+  
+        if (response) setMedias(response.results);
+        if (err) toast.error(err.message);
+      };
+  
+      getMedias();
     }, [mediaType, mediaCategory]);
+  
     return (
-        <AutoSwiper>
-            {medias.map((media, index) => (
-                <SwiperSlide key={index}>
-                    <MediaItem media={media} mediaType={mediaType}/>
-                </SwiperSlide>
-            ))}
-        </AutoSwiper>
+      <AutoSwiper>
+        {medias.map((media, index) => (
+          <SwiperSlide key={index}>
+            <MediaItem media={media} mediaType={mediaType} />
+          </SwiperSlide>
+        ))}
+      </AutoSwiper>
     );
-};
-
-export default MediaSlide;
+  };
+  
+  export default MediaSlide;
