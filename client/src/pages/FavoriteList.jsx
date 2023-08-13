476e517b404e8be02e42b5c951a6201ff3406e6f
Christopher Yeom <chrisyeom97@gmail.com>
2023-08-13 18:03:49 -0400

    starting favorites page

--- client/src/pages/FavoriteList.jsx
+++ client/src/pages/FavoriteList.jsx
@@ -1,6 +1,53 @@
-import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { LoadingButton } from '@mui/lab';
+import { Box, Button, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import MediaItem from '../components/common/MediaItem';
+import Container from '../components/common/Container';
+import uiConfigs from '../configs/ui.config';
+import favoriteApi from '../api/modules/favorite.api';
+import { setGlobalLoading } from '../redux/features/globalLoadingSlice';
+import { removeFavorite } from '../redux/features/userSlice';
+
+const FavoriteItem = ({ media, onRemoved }) => {
+    const dispatch = useDispatch();
+
+    const [onRequest, setOnRequest] = useState(false);
+
+    const onRemove = async () => {
+        if (onRequest) return;
+        setOnRequest(true);
+        const { response, err } = await favoriteApi.remove({favoriteId: media._id});
+        setOnRequest(false);
+
+        if (err) toast.error(err.message);
+        if (response) {
+            dispatch(removeFavorite({ mediaId: media.mediaId}))
+            onRemoved(media.id)
+        }
+    }
+    return (
+        <>
+            <MediaItem media={media} mediaType={media.mediaType} />
+            <LoadingButton
+                fullWidth
+                variant="contained"
+                sx={{ marginTop: 2}}
+                startIcon={<DeleteIcon />}
+                loadingPosition='start'
+                loading={onRequest}
+                onClick={onRemove}
+            >
+                remove
+            </LoadingButton>
+        </>
+    )
+}
 
 const FavoriteList = () => {
+    
     return (
         <div>
         <h1>FavoriteList Page</h1>
